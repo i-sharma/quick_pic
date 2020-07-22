e0400e11feeab091576f493571a060d9e2278f9e
Ishan <ishans996@gmail.com>
2020-07-22 17:36:35 +0530

    psycopg2 added

--- app/database.py
+++ app/database.py
@@ -7,7 +7,7 @@ SQLALCHEMY_DATABASE_URL = '***REMOVED***'
 
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL}
+    SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
