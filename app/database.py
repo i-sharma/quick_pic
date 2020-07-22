32d37d7913b335ffea2be7f873c32bf966fe4e8a
Ishan <ishans996@gmail.com>
2020-07-22 17:31:22 +0530

    minor change

--- app/database.py
+++ app/database.py
@@ -7,7 +7,7 @@ SQLALCHEMY_DATABASE_URL = '***REMOVED***'
 
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
